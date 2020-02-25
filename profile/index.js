a05b464ed962a1d43f967dfe4625b3458ee91f3f
nguyenthedat123 <57445333+nguyenthedat123@users.noreply.github.com>
2020-02-26 00:11:42 +0700

    Update index.js

--- profile/index.js
+++ profile/index.js
@@ -1,126 +1,126 @@
-import React, {useState} from 'react';
+import React,{useState} from 'react';
 import {
-    View,
+    View, 
     Text,
-    FlatList,
-    StyleSheet,
-    Image,
-    Switch,
-    Modal,
-    Button,
-    TextInput,
-    Picker
-} from 'react-native';
+     FlatList, 
+     StyleSheet, 
+     Image, 
+     Switch,
+     Modal, 
+     Button,
+     TextInput,
+     Picker
+    } from 'react-native';
 import InfoText from './info-text';
-import SubjectItem from './subject-item';
+import SubjectItem from './subject_item'
 
-export default function Profile() {
-    const userProfile = {
-        info: {
-            avatar: 'https://iap.poly.edu.vn/user/ph/PH09025.jpg',
-            name: 'Nguyen Van A',
-            email: 'a@gmail.com',
-            address: 'HN',
-            phone: '0123456789',
+export default function Profile(){
+ 
+    const userProfile={
+        info:{
+            avatar:'https://iap.poly.edu.vn/user/ph/PH09025.jpg',
+            name:'B N ',
+            email:'',
+            address:'HN',
+            phone:'4165',
             active: true
         },
-        subjects: [
+        subject:[
             {
-                name: 'React Native',
-                identity: 'MOB306',
-                className: 'PT14352',
+                name:'ReactNative1',
+                identity:'MOB306',
+                classname:'PT10111'
             },
             {
-                name: 'React Native',
-                identity: 'MOB307',
-                className: 'PT14352',
+                name:'ReactNative2',
+                identity:'MOB307',
+                classname:'PT21501'
             },
             {
-                name: 'React Native',
-                identity: 'MOB308',
-                className: 'PT14352',
+                name:'ReactNative3',
+                identity:'MOB308',
+                classname:'PT14322'
             }
         ]
     };
-    // Khai bao state + ham thay doi gia tri cua no
-    const [showInfo, setShowInfo] = useState(true);
-    // Chuyen viec su dung userprofile sang state, de khi user thay doi gia tri se render lai man hinh
-    const [user, setUser] = useState(userProfile);
-    // Tao state kiem soat viec hien thi cua modal add subject
-    const [showModal, setShowModal] = useState(false);
+    const [showModal,setShowModal]=useState(false);
 
-    // Khai bao ham thuc hien cong viec xoa
-    const handleDeleteSubject = (identity) => {
-        // su dung let de co the gan gia tri moi
-        let newSubjectList = user.subjects;
-        // filter ((item) => tra ve dieu kien ma item do se duoc xu ly)
-        // sau khi filter xong (chay het vong lap voi dieu kien dua ra) -> tra ve mang moi co cac item thoa man dk
+    const [showInfo,setShowInfo]=useState(true);
 
-        newSubjectList = newSubjectList.filter((subject) => subject.identity != identity);
-        userProfile.subjects = newSubjectList;
-        console.log(userProfile);
+    const [user, setUser]=useState(userProfile);
 
-        setUser(userProfile);
+    const [nameP,setnameP]=useState("");
+    const [identityP,setidentityP]=useState("");
+    const [classP,setclassP]=useState("");
+
+    const add=()=>{
+        let newProfilelist=user.subject;
+        const subject={
+            name: nameP.toString(),identity: identityP.toString(),
+            classname: classP.toString()
+        };
+        newProfilelist=newProfilelist.push(subject);
     }
 
-    return (
-      <View style={style.profileContainer}>
-        <View style={style.avatar}>
-          <Image style={style.image} source={{ uri: user.info.avatar }} />
-          <Switch
-            value={showInfo}
-            onValueChange={() => setShowInfo(!showInfo)}
-          />
-        </View>
-        <Text>--------------</Text>
+    const handleDeleteSub=(identity)=>{
+        let newSubjectList=user.subject;
+        newSubjectList=newSubjectList.filter((subject)=> subject.identity != identity);
+        userProfile.subject=newSubjectList;
+        setUser(userProfile);
+    }
+  
+    return(
         <View>
-          {showInfo ? <InfoText data={user.info} /> : null}
-          <Text>--------------</Text>
-          <Button
-            title="Add subject"
-            onPress={() => {
-              setShowModal(true);
-            }}
-          />
-          <FlatList
-            data={user.subjects} // mảng nhận vào để hiển thị danh sách
-            renderItem={({ item }) => (
-              <SubjectItem item={item} handleDelete={handleDeleteSubject} />
-            )} // item ứng với {name: '', identity: '', className: ''}
-            keyExtractor={(item, index) => index}
-          />
-        </View>
-        <Modal visible={showModal}>
-          <View>
-            <Text>Modal Add Subject</Text>
-            <Text>Name</Text>
-            <TextInput value="" onValueChange={() => {}} />
-            <Text>Identity</Text>
-            <TextInput value="MOBxxx" onValueChange={() => {}} />
-            <Text>Select Class Name</Text>
-            <Picker selectedValue="PT1111" onValueChange={() => {}}>
-              <Picker.Item value="PT1111" label="PT1111" />
-              <Picker.Item value="PT1112" label="PT1112" />
-              <Picker.Item value="PT1113" label="PT1114" />
-            </Picker>
-            <Button
-              title="Cancle"
-              onPress={() => {
-                setShowModal(false);
-              }}
-            />
+            <View>
+                <Image style={style.image} source={{uri: user.info.avatar}} />
+                <Switch value={showInfo} onValueChange={()=>setShowInfo(!showInfo)}/>
+            </View>
+            <Text>-------------------</Text>
+            <View>
+                {
+                    showInfo ?
+                <InfoText data={user.info}/>
+                : null
+                }   
+                <Text>------------------</Text>
+                <Button title="Add Subject" onPress={()=>{
+                    setShowModal(true);
+                }}/>
+                <FlatList
+                    data={user.subject}
+                    renderItem={({item})=><SubjectItem item={item} 
+                                handledelete={handleDeleteSub} /> }
+                    keyExtractor={(item,index)=> index}
+                />
+            </View>
+            <Modal visible={showModal}>
+                <View>
+                    <Text>Modal Add Subject</Text>
+                <Text>Name: </Text>
+                    <TextInput onChangeText={(valueName)=>setnameP(valueName)} />
+                    <Text>Identity: </Text>
+                    <TextInput onChangeText={(valIdentity)=>setidentityP(valIdentity)} />
+                    <Text>Select Class Name: </Text>
+                <Picker selectedValue="PT1234" onValueChange={() => ()}>
+                    <Picker.Item value="PT1234" label="PT1234" />
+                    <Picker.Item value="PT1235" label="PT1235" />
+                    <Picker.Item value="PT1236" label="PT1236" />
+                </Picker>
+                <Button
+                    title="Cancle"  onPress={() => {setShowModal(false);}}
+                />
+                <Button title="Subject" onPress={()=>{add(),setShowModal(false)}}/>
           </View>
         </Modal>
-      </View>
+        </View>
     );
 }
-
-const style = StyleSheet.create({
+const style=StyleSheet.create({
     profileContainer: {},
-    avatar: {},
-    image: {
-        width: 200,
-        height: 200,
-        borderRadius: 200
+    avatar:{},
+    image:{
+       width: 200,
+       height: 200,
+       borderRadius: 200 
     }
 });
